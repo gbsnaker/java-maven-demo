85e64ab93b9a3e3cd3440f712fe6a272773bcb2b
gbsnaker <longmenkezhai@sohu.com>
2020-06-02 13:51:47 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+case $1 in 
+start) 
+
+pwd 
+
+ls
+#cd /home/admin/application
+#nohup java -jar find-links.jar https://baidu.com & 
+#echo $! > pid  
+break
+;;
+stop)
+pid=`cat pid`
+kill -9 $pid 
+rm -rf /home/admin/application
+break
+;;
+*)
+  echo "Usage ${0} start or stop"
+  ;;
+esac
